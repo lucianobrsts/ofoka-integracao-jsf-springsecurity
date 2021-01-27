9cdc0a405c9d6aa6e184f567f93735c9cfaedf63
lucianobrsts <lucianobrsts@yahoo.com.br>
2021-01-27 12:30:04 -0300

    Criado o controller Index

---
+++ src/main/java/com/myimage/controller/initial/Index.java
@@ -0,0 +1,19 @@
+package com.myimage.controller.initial;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean
+public class Index {
+
+	@PostConstruct
+	public void init() {
+		System.out.println("Beam Executado...");
+	}
+
+	public String getMessage() {
+		return "Hello world JSF";
+	}
+}
